56fbdbde28f5c460bf6f7f0181a291ac162912a1
Tricia Swift <tricia.swift8@gmail.com>
2023-10-16 13:49:01 -0500

    First attempt at the expand in our url

--- views/ship_view.py
+++ views/ship_view.py
@@ -1,6 +1,7 @@
 import sqlite3
 import json
 
+
 def update_ship(id, ship_data):
     with sqlite3.connect("./shipping.db") as conn:
         db_cursor = conn.cursor()
@@ -13,12 +14,13 @@ def update_ship(id, ship_data):
                     hauler_id = ?
             WHERE id = ?
             """,
-            (ship_data['name'], ship_data['hauler_id'], id)
+            (ship_data["name"], ship_data["hauler_id"], id),
         )
 
         rows_affected = db_cursor.rowcount
 
     return True if rows_affected > 0 else False
+
 
 def delete_ship(pk):
     with sqlite3.connect("./shipping.db") as conn:
@@ -26,33 +28,54 @@ def delete_ship(pk):
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
-        db_cursor.execute("""
+        db_cursor.execute(
+            """
         DELETE FROM Ship WHERE id = ?
-        """, (pk,)
+        """,
+            (pk,),
         )
         number_of_rows_deleted = db_cursor.rowcount
 
     return True if number_of_rows_deleted > 0 else False
 
 
-def list_ships():
+def list_ships(url):
     # Open a connection to the database
     with sqlite3.connect("./shipping.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
-        db_cursor.execute("""
-        SELECT
-            s.id,
-            s.name,
-            s.hauler_id
-        FROM Ship s
-        """)
-        query_results = db_cursor.fetchall()
+        if "_expand" in url["query_params"]:
+            db_cursor.execute(
+                """
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id,
+                h.id haulerId,
+                h.name haulerName,
+                h.dock_id
+            FROM Ship s
+            JOIN Hauler h
+                ON h.id = s.hauler_id
+            """
+            )
+            query_results = db_cursor.fetchall()
+        else:
+            db_cursor.execute(
+                """
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id
+            FROM Ship s
+            """
+            )
+            query_results = db_cursor.fetchall()
 
         # Initialize an empty list and then add each dictionary to it
-        ships=[]
+        ships = []
         for row in query_results:
             ships.append(dict(row))
 
@@ -61,6 +84,7 @@ def list_ships():
 
     return serialized_ships
 
+
 def retrieve_ship(pk):
     # Open a connection to the database
     with sqlite3.connect("./shipping.db") as conn:
@@ -68,14 +92,17 @@ def retrieve_ship(pk):
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
-        db_cursor.execute("""
+        db_cursor.execute(
+            """
         SELECT
             s.id,
             s.name,
             s.hauler_id
         FROM Ship s
         WHERE s.id = ?
-        """, (pk,))
+        """,
+            (pk,),
+        )
         query_results = db_cursor.fetchone()
 
         # Serialize Python list to JSON encoded string
